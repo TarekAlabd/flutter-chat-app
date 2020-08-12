277bcab034358a2136c79895eabd9fec943aa253
TarekAlabd <tarekalabd1@gmail.com>
2020-08-12 14:53:53 +0200

    add login screen

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,82 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:firebasechatapp/widgets/login_form.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _auth = FirebaseAuth.instance;
+  bool isLoading = false;
+
+  Future<void> _submitLoginForm(
+    String email,
+    String password,
+    String username,
+    File image,
+    bool isLogin,
+    BuildContext context,
+  ) async {
+    AuthResult authResult;
+    try {
+      setState(() {
+        isLoading = true;
+      });
+      if (isLogin) {
+        authResult = await _auth.signInWithEmailAndPassword(
+            email: email, password: password);
+      } else {
+        authResult = await _auth.createUserWithEmailAndPassword(
+            email: email, password: password);
+        final ref = FirebaseStorage.instance
+            .ref()
+            .child('user_images')
+            .child(authResult.user.uid + '.jpg');
+        await ref.putFile(image).onComplete;
+        final url = await ref.getDownloadURL();
+        await Firestore.instance
+            .collection('users')
+            .document(authResult.user.uid)
+            .setData({
+          'username': username,
+          'email': email,
+          'imageUrl': url,
+        });
+      }
+    } on PlatformException catch (e) {
+      var message = 'An error occurred, please check your credentials';
+      if (e.message != null) {
+        message = e.message;
+      }
+      Scaffold.of(context).showSnackBar(
+        SnackBar(
+          content: Text(message),
+          backgroundColor: Theme.of(context).errorColor,
+        ),
+      );
+      setState(() {
+        isLoading = false;
+      });
+    } catch (e) {
+      print(e);
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: LoginForm(submitFn: _submitLoginForm, isLoading: isLoading),
+    );
+  }
+}
